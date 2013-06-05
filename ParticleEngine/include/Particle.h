6fb945f1da404ed63453e086603364a03d94dd08
fhs34785 <tpretzl.mmt-b2012@fh-salzburg.ac.at>
2013-06-05 19:43:58 +0200

    Ersten paar klassen Variblen + Methoden deklariert

--- ParticleEngine/include/Particle.h
+++ ParticleEngine/include/Particle.h
@@ -8,10 +8,19 @@ class Particle
 public:
 	Particle(void);
 	~Particle(void);
+
+	void addForce(cml::vector3f force);
+	void draw();
+	cml::vector3f getPosition();
+	cml::vector3f getVelocity();
+	void update();
 private:
 	cml::vector4f	color_;
 	cml::vector3f	force_;
 	GLboolean		isAlive_;
 	GLint			lifetime_;
-
+	GLfloat			mass_;
+	cml::vector3f	position_;
+	GLfloat			size_;
+	cml::vector3f	velocity_;
 };
--- ParticleEngine/resources/Particle.cpp
+++ ParticleEngine/resources/Particle.cpp
@@ -9,3 +9,25 @@ Particle::Particle(void)
 Particle::~Particle(void)
 {
 }
+
+void Particle::addForce(cml::vector3f force)
+{
+}
+
+void draw()
+{
+}
+
+cml::vector3f Particle::getPosition()
+{
+	return position_;
+}
+
+cml::vector3f Particle::getVelocity()
+{
+	return velocity_;
+}
+
+void update()
+{
+}
